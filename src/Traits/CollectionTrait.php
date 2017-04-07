94bbd568bc1bb62631d9c16b4d017ec8f9e8915b
Mihai Stancu <mihai.stancu@emag.ro>
2017-04-08 02:38:46 +0300

    Refactoring Callbacks as simple callable objects (via __invoke). - A convenient callable class "Each" handles invoking a method on   each object in a collection - The collection trait removes the extra parameter forwarding logic   which can be taken up by the callable object constructors.

--- src/Traits/CollectionTrait.php
+++ src/Traits/CollectionTrait.php
@@ -38,10 +38,8 @@ trait CollectionTrait
      *
      * @return array|\ArrayAccess|static
      */
-    public function walk($callback, array $parameters = [], $output = null)
+    public function walk($callback, $output = null)
     {
-        $callback = new Callback($callback);
-
         if ($output === null) {
             $output = clone $this;
             $output->exchangeArray([]);
@@ -50,7 +48,7 @@ trait CollectionTrait
         }
 
         foreach ($this as $key => $value) {
-            $output[$key] = $this->apply($callback, $parameters, $value, $key);
+            $output[$key] = $callback($value, $key);
         }
 
         return $output;
@@ -62,15 +60,14 @@ trait CollectionTrait
      *
      * @return static
      */
-    public function filter($callback = null, array $parameters = [])
+    public function filter($callback = null)
     {
-        $callback = new Callback($callback ?: [$this, 'valueIsEmpty']);
-
+        $callback = $callback ?? [$this, 'valueIsEmpty'];
         $output = clone $this;
         $output->exchangeArray([]);
 
         foreach ($this as $key => $value) {
-            if ($this->apply($callback, $parameters, $value, $key)) {
+            if ($callback($value, $key)) {
                 $output[$key] = $value;
             }
         }
@@ -143,33 +140,5 @@ trait CollectionTrait
         }
 
         return $values;
-    }
-
-    /**
-     * @param Callback $callback
-     * @param array    $parameters
-     * @param mixed    $value
-     * @param string   $key
-     *
-     * @return array
-     */
-    protected function apply(Callback $callback, array $parameters, $value, $key)
-    {
-        $callback->isCallable($value);
-        $callable = $callback->toCallable($value);
-
-        if (array_key_exists('key', $parameters)) {
-            $parameters['key'] = $key;
-        } else {
-            array_unshift($parameters, $key);
-        }
-
-        if (array_key_exists('value', $parameters)) {
-            $parameters['value'] = $value;
-        } else {
-            array_unshift($parameters, $value);
-        }
-
-        return call_user_func_array($callable, $parameters);
     }
 }
